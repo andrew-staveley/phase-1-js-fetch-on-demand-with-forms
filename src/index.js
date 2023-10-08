96de90ea0635d9a308a0e7207378de9a7bea0b0c
Andrew <andrewstaveley14@gmail.com>
2023-10-07 23:24:26 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (submitHandler));
 }
-
+function submitHandler (event) {
+    event.preventDefault();
+    event.target.children[1].value;
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+    })
+}
 document.addEventListener('DOMContentLoaded', init);
